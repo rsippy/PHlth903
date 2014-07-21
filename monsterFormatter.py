de476fdfbaf2a4aeb43b5cb89cedf86d16fa7b5b
eotles <eotles@gmail.com>
2014-07-21 16:37:01 -0500

    821

--- monsterFormatter.py
+++ monsterFormatter.py
@@ -82,7 +82,7 @@ def main():
     for line in mapFile:
         lineData = line.strip().split("\t")
         #print(len(lineData))
-        print(lineData[820])
+        print(lineData[821])
     
     
     #Kinship file
