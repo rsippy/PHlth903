be8486d9a196cf462ff3615cbe25a5838edfe057
eotles <eotles@gmail.com>
2014-07-21 15:09:45 -0500

    fixed lineData issues

--- monsterFormatter.py
+++ monsterFormatter.py
@@ -81,14 +81,10 @@ def main():
     KICFile = open(KICFilePath)
     kinFile = open(kinFilePath, "w+")
     for line in KICFile:
-        line = line.strip().split(",")
-        newLine = list()
-        newLine = line
-        print(line)
-        newLine[0] = str(famDict.getIID(lineData[0]))
-        newLine[1] = str(indDict.getIID(lineData[1]))
-        newLine[2] = str(indDict.getIID(lineData[2]))
-        print(newLine)
+        lineData = line.strip().split(",")
+        lineData[0] = str(famDict.getIID(lineData[0]))
+        lineData[1] = str(indDict.getIID(lineData[1]))
+        lineData[2] = str(indDict.getIID(lineData[2]))
         kinFile.write("\t".join(newLine) + "\n")
     KICFile.close()
     kinFile.close()
