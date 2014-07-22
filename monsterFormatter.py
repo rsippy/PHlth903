bcfa0e6082d9d937fbab49dda40139ee2e06799a
eotles <eotles@gmail.com>
2014-07-21 21:17:12 -0500

    test

--- monsterFormatter.py
+++ monsterFormatter.py
@@ -93,9 +93,8 @@ def main():
             gene = str(lineData[823])
             snp = str(lineData[1] + "_" + lineData[2])
             if (geneMap.has_key(gene) == False):
-                geneMap.update({gene : [snp]})
-            else:
-                geneMap.update({gene : geneMap.get(gene).append(snp)})
+                geneMap.update({gene : list})
+            geneMap.update({gene : geneMap.get(gene).append(snp)})
             print(gene)
             print(snp)
             print(geneMap.get(gene))
