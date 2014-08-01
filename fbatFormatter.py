8c19ed2ebdf68d26c30af1f723a8f467ecb11ac9
eotles <eotles@gmail.com>
2014-08-01 12:14:02 -0500

    new approach

--- fbatFormatter.py
+++ fbatFormatter.py
@@ -14,21 +14,37 @@ def main():
     pedFile = open(pedFilePath)
     
     header = next(genFile)
+    ids = header.strip().split("\t")
     genData = [[id, ""] for id in header.strip().split("\t")]
+    genFile.close()
+    
     
     startTime = time.clock()
     lc = 1
-    for line in genFile:
-        if(lc%1000 == 0):
-            currTime = time.clock()
-            dt = currTime - startTime
-            speed = float(lc)/float(dt)
-            remain = float(1500000-lc)/speed
-            print("%d\t%d\t%f\t%f" %(lc, dt, speed, remain))
-        lineData = line.strip().split("\t")
-        for index,lineColData in enumerate(lineData):
-            genData[index][1] += lineColData + " "
-        lc+=1
+    
+    for index,id in enumerate(ids):
+        genFile = open(genFilePath)
+        next(genFile)
+        out = id
+        sPos = 8*index
+        ePos = sPos + 3
+        for line in genFile:
+            out += line[sPos:ePos]
+        genFile.close
+        print(out)
+            
+        
+#    for line in genFile:
+#        if(lc%1000 == 0):
+#            currTime = time.clock()
+#            dt = currTime - startTime
+#            speed = float(lc)/float(dt)
+#            remain = float(1500000-lc)/speed
+#            print("%d\t%d\t%f\t%f" %(lc, dt, speed, remain))
+#        lineData = line.strip().split("\t")
+#        for index,lineColData in enumerate(lineData):
+#            genData[index][1] += lineColData + " "
+#        lc+=1
     
     lc = 0
     for stuff in genData:
