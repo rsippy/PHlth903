65ae476bf310d7d3731eb996651fe486b15d65b7
eotles <eotles@gmail.com>
2014-08-01 11:39:13 -0500

    benchmarking

--- fbatFormatter.py
+++ fbatFormatter.py
@@ -4,6 +4,8 @@ Created on Aug 1, 2014
 @author: eotles
 '''
 
+from datetime import datetime
+
 def main():
     genFilePath = "/project/EngelmanGroup/GAW19/FBAT/genonly.txt"
     pedFilePath = "/project/EngelmanGroup/GAW19/FBAT/pedonly.txt"
@@ -14,10 +16,15 @@ def main():
     header = next(genFile)
     genData = [[id] for id in header.strip().split("\t")]
     
+    startTime = datetime.now().time()
     lc = 0
     for line in genFile:
         if(lc%1000 == 0):
-            print(lc)
+            currTime = datetime.now().time()
+            dt = currTime - startTime
+            speed = float(lc)/dt
+            remain = float(1500000-lc)/speed
+            print("%d\t%d\t%f\t%f" %(lc, dt, speed, remain))
         lineData = line.strip().split("\t")
         for index,lineColData in enumerate(lineData):
             genData[index].append(lineColData.split(" "))
