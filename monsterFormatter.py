da30a738266c4f37aa1212aa7689b7b112d12f39
eotles <eotles@gmail.com>
2014-08-09 18:06:37 -0500

    add PCBP4;ABHD14B to naughty list

--- monsterFormatter.py
+++ monsterFormatter.py
@@ -151,7 +151,7 @@ def main(selectedCases, selectedControls):
     geneListFile = open(geneListFilePath, "w+")
     count = 0
     
-    naughtyList = ["SNORA62", "RBM15B;MANF", "MIR6824", "MAGI1"]
+    naughtyList = ["SNORA62", "RBM15B;MANF", "MIR6824", "MAGI1", "PCBP4;ABHD14B"]
     
     for gene,snpList in geneMap.iteritems():
         geneListFile.write(str(count) + "," + str(gene) +"\n")
